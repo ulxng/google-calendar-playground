a6ff7400865496931c0b2259185f2231f04806c7
ulxng <ulngollm@gmail.com>
2025-10-06 23:04:53 +0300

    initial

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
+)
+
+func main() {
+	keyFile := os.Getenv("SERVICE_ACCOUNT_KEY_FILE")
+	calendarID := os.Getenv("CALENDAR_ID")
+	if calendarID == "" {
+		log.Fatal("CALENDAR_ID is required")
+	}
+
+	ctx := context.Background()
+	s, err := calendar.NewService(ctx, option.WithCredentialsFile(keyFile))
+	if err != nil {
+		log.Fatalf("newService: %v", err)
+	}
+	events, err := s.Events.List(calendarID).
+		ShowDeleted(false).
+		SingleEvents(true).
+		OrderBy("startTime").
+		MaxResults(10).
+		Do()
+	if err != nil {
+		log.Fatalf("list: %v", err)
+	}
+
+	if len(events.Items) == 0 {
+		fmt.Println("no events found")
+		return
+	}
+
+	for _, event := range events.Items {
+		fmt.Printf("event: %v from %s to %s\n", event.Summary, event.Start.DateTime, event.End.DateTime)
+	}
+}
